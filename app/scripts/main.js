8f020d22924a9d205defec722ec5ddbaba8511d9
Chase Simpson <chase.w.simpson1@gmail.com>
2014-10-28 16:54:50 -0400

    js & css updatedd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,12 +5,30 @@ var Kitty = function(options) {
   this.elem = options.elem || {};
 };
 
+var lil_kitty;
 var todolist = [];
 var input_stuff;
+var item_template = $('#item_template').html();
+var rendered = _.template(item_template);
 
-$('#submit').on('click', function (event) {
+
+$('.input').on('submit', function (event) {
   event.preventDefault();
 
   input_stuff = $('#newItem').val();
-  $('.list').append("<li>" + input_stuff + "</li>");
+  // $('.list').append("<li>" + input_stuff + "</li>");
+
+  lil_kitty = new Kitty ({
+    completed: false,
+    task: input_stuff,
+    elem: $(rendered({ task: input_stuff }))[0]
+
+  }); //lil_kitty
+
+  todolist.push(lil_kitty);
+
+  $('.list').append(lil_kitty.elem);
+
+  $(this)[0].reset();
+
 });
