c821b8b35adea48a61dadc933e62b3ea4943a941
Paul Park <ppark482@gmail.com>
2014-10-29 13:56:15 -0400

    started linking to endpoint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,11 @@
+var my_server = "http://tiy-atl-fe-server.herokuapp.com/collections/chaseandpaularesoawesome";
+
+
 var Kitty = function(options) {
   options = options || {};
   this.completed = options.completed || false;
   this.deleted = options.deleted || false;
-  this.task = options.task || 'Nothing was entered';
+  this.task = options.task || '';
   this.elem = options.elem || {};
 };
 
@@ -16,28 +19,33 @@ var count = 0;
 var doneCount = 0;
 
 $('h5').html(count + ' ' + ' Zombie Kitties remaining');
+
 // Creates a new kitty (list) item
 $('.input').on('submit', function (event) {
   event.preventDefault();
-
+  var self = this;
   input_stuff = $('#newItem').val();
   // $('.list').append("<li>" + input_stuff + "</li>");
-
   lil_kitty = new Kitty ({
     completed: false,
     task: input_stuff,
-    elem: $(rendered({ task: input_stuff }))[0]
-
+    elem: $(rendered( {task: input_stuff} ))[0]
   }); //lil_kitty
-
   todolist.push(lil_kitty);
-
-  $('.list').append(lil_kitty.elem);
-
-  $(this)[0].reset();
+  // Send to our server
+  $.ajax({ // content is assumed to be JSON
+    type: 'POST',
+    url:  my_server,
+    data: lil_kitty // sends data in
+    }).done( function (data) {
+      $('.list').append(lil_kitty.elem);
+      $(self)[0].reset();
+  // render to todo
+  // this allows you to wait to send to server before adding to local arrays
+  // server creates the unique id, you want to add that to your arrays
+  });
 
   // How many kittys?
-
   count += 1;
   $('h5').html(count + ' ' + ' Zombie Kitties remaining');
 
