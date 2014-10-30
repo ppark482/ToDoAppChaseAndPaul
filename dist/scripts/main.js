c21a65c6c673e157dffd79d5ef96ed63f392a623
Paul Park <ppark482@gmail.com>
2014-10-30 08:46:56 -0400

    gulp

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,78 +1,167 @@
+// Delete button animation
+$('ul').on('mouseover', '.delete', function(event) {
+  event.preventDefault();
+  $(this).css('padding-left', '65px').html('delete');
+  $('ul').on('mouseout', '.delete', function(event) {
+    event.preventDefault();
+    $(this).css('padding-left','').html('X');
+  })
+});
 
-var my_server = "http://tiy-atl-fe-server.herokuapp.com/collections/chaseandpaularesoawesome";
-
-
+var url = 'http://tiy-atl-fe-server.herokuapp.com/collections/zombiekitty2';
+// Item Constructor
+// --------------------------------------------------------------------------------------------------------------------------------------------
 var Kitty = function(options) {
   options = options || {};
-  this.completed = options.completed || false;
-  this.deleted = options.deleted || false;
-  this.task = options.task || '';
-  this.elem = options.elem || {};
+  this.deleted = 'false';
+  this.task = options.task || 'Nothing was entered';
+  this.done = 'false';
 };
-
+// Variables
+// --------------------------------------------------------------------------------------------------------------------------------------------
 var lil_kitty;
-var todolist = [];
-
+var todolist;
 var input_stuff;
 var item_template = $('#item_template').html();
 var rendered = _.template(item_template);
 var count = 0;
-var doneCount = 0;
+var deleteCount = 0;
+var taskCompleted = 0;
 
+// Grabbing all ToDo items and showing on page
+// --------------------------------------------------------------------------------------------------------------------------------------------
+$.getJSON(url).done( function (data) {
+
+  todolist = data;
+
+  _.each(todolist, function (i) {
+    if (i.deleted === 'true') {
+      deleteCount += 1;
+    } else if (i.deleted === 'false'){
+      count += 1;
+      $('.list').append(rendered(i));
+      count = (todolist.length - deleteCount)
+    }
+  });
+  // count = (todolist.length);
+  $('h5').html(count + ' ' + ' Zombie Kitties remaining');
+});
+
+// List Item Count
+// --------------------------------------------------------------------------------------------------------------------------------------------
 $('h5').html(count + ' ' + ' Zombie Kitties remaining');
 
 // Creates a new kitty (list) item
+// --------------------------------------------------------------------------------------------------------------------------------------------
 $('.input').on('submit', function (event) {
   event.preventDefault();
   var self = this;
+
   input_stuff = $('#newItem').val();
-  // $('.list').append("<li>" + input_stuff + "</li>");
+
   lil_kitty = new Kitty ({
     completed: false,
     task: input_stuff,
-    elem: $(rendered( {task: input_stuff} ))[0]
+
   }); //lil_kitty
-  todolist.push(lil_kitty);
-  // Send to our server
-  $.ajax({ // content is assumed to be JSON
+
+  //Send to our server
+  // --------------------------------------------------------------------------------------------------------------------------------------------
+  $.ajax({
     type: 'POST',
-    url:  my_server,
-    data: lil_kitty // sends data in
-    }).done( function (data) {
-      $('.list').append(lil_kitty.elem);
-      $(self)[0].reset();
-  // render to todo
-  // this allows you to wait to send to server before adding to local arrays
-  // server creates the unique id, you want to add that to your arrays
+    url: url,
+    data: lil_kitty
+  }).done( function (data) {
+
+    //Add to todolist
+    // -----------------------------------------------------------------------------------------------------------------------------------------
+    todolist.push(data);
+
+    //Show our task on the page
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+    $('.list').append(rendered(data));
+
+    //Reset my form
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+    $(self)[0].reset();
+
   });
 
   // How many kittys?
+  // --------------------------------------------------------------------------------------------------------------------------------------------
   count += 1;
   $('h5').html(count + ' ' + ' Zombie Kitties remaining');
 
-});
+}); // end creating new list item
 
-// Mark Item As Done
-$('ul').on('click', 'li', function (event) {
+// Modifier
+// --------------------------------------------------------------------------------------------------------------------------------------------
+$('.list').on('click', 'li', function (event) {
   event.preventDefault();
-  $(this).addClass('completed');
-  doneCount += 1;
-});
-// Unmark Item as Done
-$('ul').on('click', '.completed', function (event) {
-  event.preventDefault();
-  $(this).removeClass('completed');
-  doneCount -= 1;
-});
+  // Take ID # from rendered item
+  var ID = $(this).attr('id');
+
+  // Find the instance of my task
+  var modifier = _.findWhere(todolist, { _id: ID });
+  // If it's done, mark it undone, else mark it done
+  if (modifier.done === 'true') {
+    modifier.done = 'false';
+    $(this).removeClass('done');
+    $('.allDone').css('color', 'HSLA(97, 57%, 68%, 1)');
+    $(this).css('background-color','');
+  } else {
+    modifier.done = 'true';
+    $(this).addClass('done');
+    // $(this).append('<div class="allDone">COMPLETED</div>');
+    $('.list').append(this);
+    $(this).css('background-color', 'HSLA(122, 98%, 22%, 1)');
+  }
+  // ajax call to update completed status
+  $.ajax({
+    type: 'PUT',
+    url: url + "/" + modifier._id,
+    data: modifier
+  });
+}); //Modifier $(.list)
+
+// // Mark Item As Done
+// $('ul').on('click', 'li', function (event) {
+//   event.preventDefault();
+//   $(this).addClass('completed');
+//   doneCount += 1;
+// });
+// // Unmark Item as Done
+// $('ul').on('click', '.completed', function (event) {
+//   event.preventDefault();
+//   $(this).removeClass('completed');
+//   doneCount -= 1;
+// });
+
 // Deletes a bad kitty
-$('.list').on('click', '#delete', function() {
-  $(this).delay( 1000 ).parent().remove();
-  count -=1;
+// --------------------------------------------------------------------------------------------------------------------------------------------
+$('#clearCompleted').on('click', function(event) {
+  event.preventDefault();
+  console.log('clicked');
+  $('.list').html('');
+  _.each(todolist, function(item) {
+    item.deleted = 'true';
+    var ID = item._id;
+
+    if (item.deleted==='true') {
+      $.ajax({
+        type: 'DELETE',
+        url: url + "/" + ID,
+        data: item
+      });
+    };
+
+  });
+  count = 0;
   $('h5').html(count + ' ' + ' Zombie Kitties remaining');
 });
 
-//party kitty
+// party kitty
+// --------------------------------------------------------------------------------------------------------------------------------------------
 $('.party-kitty').hover(function () {
   $(this).toggleClass('animated shake');
-  $('.list').toggleClass('animated shake');
 });
